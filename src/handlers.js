e0ce7642814a3e098b067462350e51707e085a11
Bernard Nehal D'Cunha <58025656+bernie-walker@users.noreply.github.com>
2020-07-19 21:44:11 +0530

    modified processAuthCode to register the session

--- src/handlers.js
+++ src/handlers.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-let sessionID = 0;
+let id = 0;
 const sessions = {};
 
 const clientID = 'f208c210845e35eb5151';
@@ -11,26 +11,67 @@ const logRequest = function (req, res, next) {
   next();
 };
 
+const insertUserName = function (userName) {
+  sessions[++id] = userName;
+  return id;
+};
+
+const getUserInfo = function (accessToken) {
+  return axios.get('https://api.github.com/user', {
+    headers: {
+      Authorization: `token ${accessToken}`,
+      accept: 'application/json',
+    },
+  });
+};
+
+const getAccessToken = function (code) {
+  return axios({
+    method: 'post',
+    url: `https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${code}`,
+    headers: {
+      accept: 'application/json',
+    },
+  });
+};
+
+const registerSession = async function (accessCode) {
+  let userName;
+  const { access_token: accessToken } = await getAccessToken(accessCode);
+
+  if (accessToken) {
+    const userInfo = getUserInfo(accessToken);
+    userName = userInfo.login;
+  }
+
+  if (userName) {
+    return insertUserName(userName);
+  } else {
+    throw 'Unauthorized';
+  }
+};
+
 const processAuthCode = function (req, res) {
-  console.log(req.query.code);
-  res.redirect('/userPage');
+  registerSession(req.query.code)
+    .then((sessionID) => {
+      res.cookie('sesID', sessionID);
+      res.redirect('/userPage');
+    })
+    .catch(() => {
+      res.status(401).end();
+    });
 };
 
 const renderUserPage = function (req, res) {
-  res.render('user', { name: 'default' });
-};
+  const { sesID } = req.cookie;
+  const userName = sessions[sesID];
 
-// axios({
-//   method: 'post',
-//   url: `https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=cbca964bbf1f81462d77`,
-//   headers: {
-//     accept: 'application/json',
-//   },
-// }).then((response) => {
-//   console.log(response.data);
-// });
-// .catch((err) => {
-//   console.log('err', err);
-// });
+  if (userName) {
+    res.status(401).end();
+    return;
+  }
+
+  res.render('user', { userName });
+};
 
 module.exports = { logRequest, processAuthCode, renderUserPage };
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const { logRequest, processAuthCode, renderUserPage } = require('./handlers');
 const app = express();
 
 app.set('view engine', 'pug');
+app.use(cookieParser());
 app.use(logRequest);
 app.use(express.static('public'));
 
